364cb110441632a136effbca5ded9758a278bb03
cy <15652338313@163.com>
2022-02-27 14:47:17 +0800

    脚手架初识React

--- staging/react-staging/src/App.js
+++ staging/react-staging/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Hellow from "./components/Hello/Hello.js"
 function App() {
   return (
     <div className="App">
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!!!
         </a>
+        <Hellow />
       </header>
     </div>
   );
---
+++ staging/react-staging/src/components/Hello/Hello.js
@@ -0,0 +1,9 @@
+import {Component} from "react"
+import "./Hello.css"
+export default class Hellow extends Component{
+    render(){
+        return(
+            <h1 className="title">Hello,组件</h1>
+        )
+    }
+}
