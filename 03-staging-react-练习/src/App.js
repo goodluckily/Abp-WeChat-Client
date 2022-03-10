5078ebdad9c4c0be3ec8c4f0f5a4fe9ca408e788
cy <15652338313@163.com>
2022-03-10 20:52:34 +0800

    0310

--- 03-staging-react-练习/src/App.js
+++ 03-staging-react-练习/src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link, NavLink, Switch, Redirect } from "react-router-dom";
-import { Button } from "antd";
 import { WechatOutlined } from "@ant-design/icons";
+import Count from "./components/Count";
+
 import "./App.less";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Button type="primary">Primary Button</Button>
-        <Button type="link">Primary Button</Button>
-        <Button type="text">Primary Button</Button>
-        <Button type="ghost">Primary Button</Button>
-        <Button type="dashed">Primary Button</Button>
-        <Button type="default ">Primary Button</Button>
-        <WechatOutlined style={{ fontSize: "16px", color: "#08c" }} />
+        <Count>yarn</Count>
       </div>
     );
   }
